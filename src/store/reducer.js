d2d4075ee79048c80d8b1d235f1bd8219b09cf88
goseeya <149542@gmail.com>
2019-08-25 16:17:54 +0200

    add decrement, add, subtract actions

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,14 +1,29 @@
 const initialState = {
-  counter: 0
+    counter: 0
 }
 
 const reducer = (state = initialState, action) => {
-  if (action.type === 'INCREMENT') {
-    retrun {
-      counter: state.counter + 1
+    if (action.type === 'INCREMENT') {
+        return {
+            counter: state.counter + 1
+        }
     }
-  }
-  return state;
+    if (action.type === 'DECREMENT') {
+        return {
+            counter: state.counter - 1
+        }
+    }
+    if (action.type === 'ADD') {
+        return {
+            counter: state.counter + action.value
+        }
+    }
+    if (action.type === 'SUBTRACT') {
+        return {
+            counter: state.counter - action.value
+        }
+    }
+    return state;
 };
 
 export default reducer;
